1db98c9a701364610fd4fe7ffd729c724d1a4650
Ruchika Patro <82655296+patroruchika@users.noreply.github.com>
2021-04-18 12:54:40 +0530

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,20 @@
+i1 = int(input())
+d1 = float(input())
+s1 = str(input())
+
+sum_i = i + i1
+sum_d = d + d1
+sum_s = s + s1
+print(sum_i)
+print(sum_d)
+print(sum_s)
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
